2c603b1c61685fa6353ab0fc83852c6dee2674c8
Naes <naesdevenecia88@gmail.com>
2022-09-24 11:39:42 -0500

    edited for angular routing

--- starwarsAPI/src/app/app.module.ts
+++ starwarsAPI/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { SpeciesComponent } from './species/species.component';
+import { AppRoutingModule } from './app-routing.module';
+import { VehiclesComponent } from './vehicles/vehicles.component';
 
 @NgModule({
-  declarations: [AppComponent, SpeciesComponent],
-  imports: [BrowserModule, HttpClientModule],
+  declarations: [AppComponent, SpeciesComponent, VehiclesComponent],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
